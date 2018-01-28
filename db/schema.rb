9ba8a369499873fb78c51e3304f7cd85b019d676
Rob <github email address>
2018-01-28 09:42:03 -0500

    schema currently correct

---
+++ db/schema.rb
@@ -0,0 +1,72 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180128143856) do
+
+  create_table "item_categories", force: :cascade do |t|
+    t.integer  "maintenance_item_id"
+    t.integer  "maintenance_category_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "item_categories", ["maintenance_category_id"], name: "index_item_categories_on_maintenance_category_id"
+  add_index "item_categories", ["maintenance_item_id"], name: "index_item_categories_on_maintenance_item_id"
+
+  create_table "maintenance_categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "maintenance_items", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "maintenance_logs", force: :cascade do |t|
+    t.string   "notes"
+    t.string   "tools"
+    t.integer  "cost"
+    t.datetime "date_performed"
+    t.datetime "date_due"
+    t.integer  "maintenance_category_id"
+    t.integer  "maintenance_item_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "maintenance_logs", ["maintenance_category_id"], name: "index_maintenance_logs_on_maintenance_category_id"
+  add_index "maintenance_logs", ["maintenance_item_id"], name: "index_maintenance_logs_on_maintenance_item_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+end
