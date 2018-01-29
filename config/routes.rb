f5656f904832dd8ad3fc23397a5f6e74f69374ce
Rob <github email address>
2018-01-29 06:14:13 -0500

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :maintenance_items
   resources :maintenance_categories
   devise_for :users
-  root "home#index"
+  
+  root "maintenance_items#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
