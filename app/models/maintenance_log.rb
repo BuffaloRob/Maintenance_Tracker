d0af184eb94c001a15e4a7f5446eb228e2de8e29
BuffaloRob <peckham.rob@gmail.com>
2018-01-30 09:34:02 -0500

    stubbed out methods that can be useful

--- app/models/maintenance_log.rb
+++ app/models/maintenance_log.rb
@@ -1,4 +1,12 @@
 class MaintenanceLog < ActiveRecord::Base
   belongs_to :maintenance_category
   belongs_to :maintenance_item
+
+  def formatted_time
+    strftime("%b %d, %Y")
+  end
+
+  def time_entered?
+    #returns true if date_performed/date_due exists
+  end
 end
