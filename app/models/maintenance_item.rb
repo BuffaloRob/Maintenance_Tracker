07a6fa13fe6bbb67050b31087ee384e7c615e055
BuffaloRob <peckham.rob@gmail.com>
2018-01-31 10:09:10 -0500

    set-up associations

--- app/models/maintenance_item.rb
+++ app/models/maintenance_item.rb
@@ -1,2 +1,6 @@
 class MaintenanceItem < ActiveRecord::Base
+  belongs_to :user
+  has_many :item_categories
+  has_many :maintenance_categories, :through => :item_categories
+  has_many :maintenance_logs
 end
