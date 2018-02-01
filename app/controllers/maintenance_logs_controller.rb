f0a6cd473f69698e3c9cc0b04de9587afa90e240
BuffaloRob <peckham.rob@gmail.com>
2018-02-01 13:29:08 -0500

    made compatible with nested resources/routes

--- app/controllers/maintenance_logs_controller.rb
+++ app/controllers/maintenance_logs_controller.rb
@@ -1,12 +1,25 @@
 class MaintenanceLogsController < ApplicationController
-  before_action :set_maintenance_log, only: [:show, :edit, :update, :destroy]
+  # before_action :set_maintenance_log, only: [:show, :edit, :update, :destroy]
 
   def index
-    @maintenance_logs = MaintenanceLog.all
+    if params[:maintenance_category_id]
+      @maintenance_category = MaintenanceCategory.find_by(id: params[:maintenance_category_id])
+      if @maintenance_category.nil?
+        redirect_to maintenance_category_path, alert: "That category doesn't exist"
+      else
+        @maintenance_logs = @maintenance_category.maintenance_logs
+      end
+    else 
+      @maintenance_logs = MaintenanceLog.all
+    end
   end
 
   def new
-    @maintenance_log = MaintenanceLog.new
+    if params[:maintenance_category_id] && !MaintenanceCategory.exists?(params[:maintenance_category_id])
+      redirect_to maintenance_category_path, alert: "that category doesn't exist"
+    else 
+      @maintenance_log = MaintenanceLog.new(maintenance_category_id: params[:maintenance_category_id])
+    end
   end
 
   def create
@@ -14,14 +27,19 @@ class MaintenanceLogsController < ApplicationController
     if @maintenance_log.save
       redirect_to maintenance_log_path(@maintenance_log)
     else
-      @maintenance_logs = MaintenanceLog.all
-      redirect_to :index
+      render :new
     end
   end
 
   def update
-    @maintenance_log.update(maintenance_log_params)
-    redirect_to maintenance_log_path(@maintenance_log)
+    @maintenance_log = MaintenanceLog.find(params[:id])
+    @maintenance_log.update
+
+    if @maintenance_log.save
+      redirect_to maintenance_log_path(@maintenance_log)
+    else 
+      render :edit
+    end
   end
 
   def destroy
@@ -30,16 +48,36 @@ class MaintenanceLogsController < ApplicationController
   end
 
   def show
+    if params[:maintenance_category_id]
+      @maintenance_category = MaintenanceCategory.find_by(id: params[:maintenance_category_id])
+      @maintenance_log = @maintenance_category.maintenance_logs.find_by(id: params[:id])
+      if @maintenance_log.nil?
+        redirect_to maintenance_category_maintenance_log_path(@maintenance_category) alert: "log not found"
+      end 
+    else
+      @maintenance_log = MaintenanceLog.find(params[:id])
+    end
   end
 
   def edit 
+    if params[:maintenance_category_id]
+      maintenance_category = MaintenanceCategory.find_by(id: params[:maintenance_category_id])
+      if maintenance_category.nil?
+        redirect_to maintenance_category_path, alert: "category not found"
+      else 
+        @maintenance_log = maintenance_category.maintenance_logs.find_by(id: params[:id])
+        redirect_to maintenance_category_maintenance_logs_path, alert: "log not found" if @maintenance_log.nil?
+      end 
+    else 
+      @maintenance_log = MaintenanceLog.find(params[:id])
+    end 
   end
 
   private
 
-  def set_maintenance_log
-    @maintenance_log = MaintenanceLog.find(params[:id])
-  end
+  # def set_maintenance_log
+  #   @maintenance_log = MaintenanceLog.find(params[:id])
+  # end
 
   def maintenance_log_params
     params.require(:maintenance_log).permit(:notes, :tools, :cost, :date_performed, :date_due)
