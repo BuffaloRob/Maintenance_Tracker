e28955194cd3d99f105ee952b1b6b8beb1f5a043
Rob <github email address>
2018-02-02 06:18:39 -0500

    added comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :maintenance_items
+  has_many :maintenance_items # Do I want #dependent: :destroy  here? I'd like for all of a users records to be deleted if they delete their profile. Does this just destroy the associated object, MaintenanceItem ? Or does it also destroy the objects associated to MaintenanceItem?
+  
 end
