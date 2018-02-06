004244c10f42010ef1d7e5ac80f9ccc387194d11
BuffaloRob <peckham.rob@gmail.com>
2018-02-06 13:04:38 -0500

    changed data to match new schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+MaintenanceItem.destroy_all
+MaintenanceCategory.destroy_all
+MaintenanceLog.destroy_all
+
+tom = User.create(email: 'tom@tom.com', password: 'password', password_confirmation: 'password')
+
+car = MaintenanceItem.create(name: 'GT86', user_id: 1 )
+
+oil_change = MaintenanceCategory.create(name: 'Oil Change', maintenance_item_id: 1)
+
+log1 = MaintenanceLog.create(date_performed: 10.days.ago, date_due: Time.now, tools: 'wrench, rags, drip pan', cost: 40, notes: 'sample note', maintenance_category_id: 1)
