2bf5b7049204fd50a775fc69fc98b2c50c06114b
BuffaloRob <peckham.rob@gmail.com>
2018-02-06 15:14:54 -0500

    added past_due and upcoming class methods

--- app/models/maintenance_log.rb
+++ app/models/maintenance_log.rb
@@ -2,5 +2,14 @@ class MaintenanceLog < ActiveRecord::Base
   belongs_to :maintenance_category
   belongs_to :maintenance_item
 
+  def self.past_due
+    #returns list of all logs that are past due
+    where("date_due <=?", Time.zone.today.end_of_day)
+  end
+
+  def self.upcoming
+    #returns list of all logs that'll be due in the next month
+    where("date_due <=?", Time.now + 30.days)
+  end
 
 end
