5b620aa2f11d7ab566898122c7067adddcb23128
BuffaloRob <peckham.rob@gmail.com>
2018-02-07 12:53:22 -0500

    changed associations to fit new schema

--- app/models/maintenance_item.rb
+++ app/models/maintenance_item.rb
@@ -1,5 +1,5 @@
 class MaintenanceItem < ActiveRecord::Base
   belongs_to :user
-  has_many :maintenance_categories
-  has_many :maintenance_logs, :through => :maintenance_categories
+  has_many :maintenance_categories, :through => :maintenance_logs
+  has_many :maintenance_logs
 end
