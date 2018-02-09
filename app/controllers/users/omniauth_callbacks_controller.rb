4ccab94e8327f223b81ac2f7f47168950f96b7c7
BuffaloRob <peckham.rob@gmail.com>
2018-02-09 15:32:15 -0500

    changed facbook method

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,17 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
+  # def facebook
+  #   if current_user.present?
+  #     current_user.apply_omniauth(request.env["omniauth.auth"])
+  #     redirect_to edit_user_registration_path, notice: "Facebook Account Linked!"
+  #   else
+  #     @user = User.from_omniauth(request.env["omniauth.auth"])
+  #     sign_in_and_redirect @user
+  #   end
+  # end
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    sign_in_and_redirect @user      
   end
+
 
   def google
     @user = User.from_omniauth(request.env["omniauth.auth"])
