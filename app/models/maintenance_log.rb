95ac92661f742699df89d2b574789ebac5ceb039
Rob <github email address>
2018-02-10 07:44:25 -0500

    added method to help find the current_users logs instead of all logs from all users

--- app/models/maintenance_log.rb
+++ app/models/maintenance_log.rb
@@ -1,9 +1,10 @@
 class MaintenanceLog < ActiveRecord::Base
   belongs_to :maintenance_category
   belongs_to :maintenance_item
-  accepts_nested_attributes_for :maintenance_category #reject_if: category_blank?
+  # accepts_nested_attributes_for :maintenance_category, reject_if: category_blank?
 
   def maintenance_category_attributes=(category_attributes)
+    #check to make sure either radio pressed or name typed in, can only be one or the other
     category = MaintenanceCategory.find_or_create_by(category_attributes)
     self.maintenance_category = category 
   end
@@ -20,6 +21,17 @@ class MaintenanceLog < ActiveRecord::Base
   def self.upcoming
     #returns list of all logs that'll be due in the next month
     where("date_due <=?", Time.now + 30.days)
+  end
+
+  def self.current_user_logs
+    @current_logs=[]
+    @items = current_user.maintenance_items.all
+    @items.each do |item|
+      @logs = item.maintenance_logs.all
+      @logs.each do |log|
+       @current_logs << log
+      end
+    end
   end
 
 end
