027d0bdf1f103365f5c1993c7ebc58260b4873aa
BuffaloRob <peckham.rob@gmail.com>
2018-02-19 09:06:54 -0500

    added dependent: :destroy

--- app/models/maintenance_item.rb
+++ app/models/maintenance_item.rb
@@ -1,6 +1,6 @@
 class MaintenanceItem < ActiveRecord::Base
   belongs_to :user
   has_many :maintenance_categories, :through => :maintenance_logs
-  has_many :maintenance_logs
+  has_many :maintenance_logs, :dependent => :destroy
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
-  has_many :maintenance_items # Do I want #dependent: :destroy  here? I'd like for all of a users records to be deleted if they delete their profile. Does this just destroy the associated object, MaintenanceItem ? Or does it also destroy the objects associated to MaintenanceItem?
+  has_many :maintenance_items, :dependent => :destroy
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
