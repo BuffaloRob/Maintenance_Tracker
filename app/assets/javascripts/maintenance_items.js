b99a5e517dc136e170d14a783dc94c9e0b67e652
BuffaloRob <peckham.rob@gmail.com>
2018-05-09 15:25:41 -0400

    jquery call now logs return data

--- app/assets/javascripts/maintenance_items.js
+++ app/assets/javascripts/maintenance_items.js
@@ -1,4 +1,18 @@
-$("[id*=catBtn]").on("click", function(event) {
-  event.preventDefault();
-  alert("yup");
-});
+$(document).on('turbolinks:load', function() {
+
+  $("[id*=logBtn]").on("click", function (event) {
+    event.preventDefault();
+    let logPath = event.target.pathname;
+
+  $.getJSON(logPath)
+    .done(function(data) {
+      console.log(data);
+    })
+    .fail(function(data) {
+      console.log("Error:");
+      console.log(data);
+    });
+  });
+
+})
+  
