5a4474045301eeff494db265b9770464f0f88248
BuffaloRob <peckham.rob@gmail.com>
2018-05-16 13:25:31 -0400

    jQuery call now iterates over and displays all logs if show logs pressed

--- app/assets/javascripts/maintenance_items.js
+++ app/assets/javascripts/maintenance_items.js
@@ -6,21 +6,23 @@ $(document).on('turbolinks:load', function() {
 
   $.getJSON(logPath)
     .done(function(data) {
-      let result = "";
-      let id = data[0].maintenance_category_id;
-      let note = data[0].notes;
-      let date_performed = data[0].date_performed;
-      let date_due = data[0].date_due;
-      let tools = data[0].tools;
-      let log = $("#showLog_" + id);
 
-      result += "<p>Performed on: " + date_performed + "</p>" + "\n" + 
-      "<p>Due on: " + date_due + "</p>" + "\n" +
-      "<p>Notes: " + note + "</p>";
+      for (obj of data) {
+        let result = "";
+        let id = obj.maintenance_category_id;
+        let note = obj.notes;
+        let date_performed = obj.date_performed;
+        let date_due = obj.date_due;
+        let tools = obj.tools;
+        let log = $("#showLog_" + id);
 
-      log.append(result);
+        result += "<p>Performed on: " + date_performed + "</p>" + "\n" +
+          "<p>Due on: " + date_due + "</p>" + "\n" +
+          "<p>Notes: " + note + "</p>" +
+          "<hr>";
 
-      console.log(data);
+        log.append(result);
+      }
     })
     .fail(function(data) {
       console.log("Error:");
