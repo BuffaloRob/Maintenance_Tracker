314e1b0765bad4ddaf16732f41212cc59b3d083b
BuffaloRob <peckham.rob@gmail.com>
2018-05-22 14:25:02 -0400

    added v1 scripts for items index page

--- app/assets/javascripts/maintenance_items.js
+++ app/assets/javascripts/maintenance_items.js
@@ -1,41 +1,37 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', function () {
 
-  $("[id*=logBtn]").one("click", function (event) {
+  $(".showCat").one("click", function (event) {
     event.preventDefault();
-    let logPath = event.target.pathname;
+    let categoryPath = event.target.pathname;
 
-  $.getJSON(logPath)
-    .done(function(data) {
+    $.getJSON(categoryPath)
+      .done(function (data) {
+        console.log(data)
 
-      for (obj of data) {
-        let result = "";
-        let id = obj.maintenance_category_id;
-        let note = obj.notes;
-        let date_performed = obj.date_performed;
-        let date_due = obj.date_due;
-        let tools = obj.tools;
-        let $log = $("#showLog_" + id);
+        for (obj of data) {
+          let result = "";
+          let id = obj.id;
+          let name = obj.name;
+          let $category = $("showCategory_" + id);
 
-        result += "<p>Performed on: " + date_performed + "</p>" + "\n" +
-          "<p>Due on: " + date_due + "</p>" + "\n" +
-          "<p>Notes: " + note + "</p>" +
+          result += "<p>" + name + "</p>" +
           "<hr>";
 
-        $log.append(result);
-      }
-    })
-    .fail(function(data) {
-      console.log("Error:");
-      console.log(data);
-    });
+          $category.append(result);
+        }
+      })
+      .fail(function (data) {
+        console.log("Error:");
+        console.log(data);
+      });
   });
 
-    $(".accordion").accordion({
-      collapsible: true,
-      active: false,
-    });
+  $(".accordion").accordion({
+    collapsible: true,
+    active: false,
+  });
 
-  $("[id*=logBtn]").on("click", function (event) {
+  $(".showCat").on("click", function (event) {
     event.preventDefault();
   })
 
