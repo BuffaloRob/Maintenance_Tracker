422d41c19ee80476463b5cc509b4350246524170
BuffaloRob <peckham.rob@gmail.com>
2018-05-25 11:36:49 -0400

    added listener for new log form at bottom of maint items show page

--- app/assets/javascripts/maintenance_categories.js
+++ app/assets/javascripts/maintenance_categories.js
@@ -39,6 +39,23 @@ $(document).on('turbolinks:load', function () {
     event.preventDefault();
   })
 
+  //For new log form on Maintenance Category Show page
+  $('form').submit(function (event) {
+    event.preventDefault();
+    let values = $(this).serialize();
+
+    // I don't think 'maint_categories' is the right route to use below
+    // Also, what controller and what controller action should this be going through? Currently using the maintenance_items show controller
+    let logs = $.post('/maintenance_categories', values);
+
+    logs.done(function (data) {
+      console.log(data);
+      let log = data;
+      $("#logDatePerformed").text(post["date_performed"]);
+      $("#logNotes").text(post["notes"]);
+    });
+  });
+
 })
 
 
