6e35358f4d2c927df29d2cfd883781f974c2396a
BuffaloRob <peckham.rob@gmail.com>
2018-06-14 12:38:37 -0400

    call now uses fetch and a class constructor, old way commented out

--- app/assets/javascripts/maintenance_logs.js
+++ app/assets/javascripts/maintenance_logs.js
@@ -3,32 +3,42 @@ $(document).on('turbolinks:load', function () {
   $("[id*=detailsBtn]").one("click", function (event) {
     event.preventDefault();
     const detailsPath = event.target.pathname;
-    
-    $.getJSON(detailsPath)
-      .done(function (data) {
 
-        let result = "";
-        const id = data.id;
-        const note = data.notes;
-        const date_performed = new Date(data.date_performed);
-        const date_due = new Date(data.date_due);
-        const tools = data.tools;
-        const cost = data.cost;
-        const $details = $("#showDetails_" + id);
+    // $.getJSON(detailsPath)
+    //   .done(function (data) {
 
-        result += 
-          "<p>Performed on: " + date_performed.toLocaleDateString('en-US', {timeZone: 'UTC'}) + "</p>" + "\n" +
-        "<p>Due on: " + date_due.toLocaleDateString('en-US', { timeZone: 'UTC' }) + "</p>" + "\n" +
-          "<p>Notes: " + note + "</p>" + "\n" +
-          "<p>Cost: $" + cost + "</p>" + "\n" +
-          "<p>Tools: " + tools + "</p>" +
-          "<hr>";
+    //     let result = "";
+    //     const id = data.id;
+    //     const note = data.notes;
+    //     const date_performed = data.date_performed;
+    //     const date_due = data.date_due;
+    //     const tools = data.tools;
+    //     const cost = data.cost;
+    //     const $details = $("#showDetails_" + id);
 
-        $details.append(result);
+    //     result += 
+    //       "<p>Performed on: " + date_performed + "</p>" + "\n" +
+    //       "<p>Due on: " + date_due + "</p>" + "\n" +
+    //       "<p>Notes: " + note + "</p>" + "\n" +
+    //       "<p>Cost: $" + cost + "</p>" + "\n" +
+    //       "<p>Tools: " + tools + "</p>" +
+    //       "<hr>";
+
+    //     $details.append(result);
+    //   })
+    //   .fail(function (data) {
+    //     console.log("Error:");
+    //     console.log(data);
+    //   });
+
+    fetch(detailsPath)
+      .then(function (resp) {
+        return resp
+
       })
-      .fail(function (data) {
-        console.log("Error:");
-        console.log(data);
+      .then(function (data) {
+        const logDetails = new LogDetails(data.notes, data.tools);
+        console.log(logDetails.renderDetails());
       });
   });
 
@@ -37,10 +47,20 @@ $(document).on('turbolinks:load', function () {
     active: false,
   });
 
-  $("[id*=detailsBtn]").on("click", function(event) {
+  $("[id*=detailsBtn]").on("click", function (event) {
     event.preventDefault();
   });
 
 })
 
 
+class LogDetails {
+  constructor(notes, tools) {
+    this.notes = notes;
+    this.tools = tools;
+  }
+
+  renderDetails() {
+    console.log(`these are the deetz ${this.notes} and ${this.tools}.`)
+  }
+}
